eb7cba1d75de651039c7f8019f2ffeeea9c9a78e
GekoPlay <97613551+GekoPlay@users.noreply.github.com>
2026-02-26 16:54:25 +0100

    maj

--- PROG FONCTIONNELLE/Annales/Annales 2024.ml
+++ PROG FONCTIONNELLE/Annales/Annales 2024.ml
@@ -72,3 +72,59 @@ let score_set  = function  marque_match ->
   let j1 = nb_occur "1" marque_match in
   let j2 = nb_occur "2" marque_match in
   string_of_int(j1)^string_of_int(j2);;
+
+let rec prem_set = function marque_match ->
+  if est_vide marque_match || prem_lettre marque_match = " " then 
+    ""
+  else
+      prem_lettre marque_match ^ prem_set(reste_mot marque_match);; 
+
+let rec reste_set = function marque_match ->
+  if est_vide marque_match then
+    ""
+  else
+    if prem_lettre marque_match = " " then
+      reste_mot marque_match
+    else
+      reste_set (reste_mot marque_match);;
+
+
+let j_gagne = function score_match_ch ->
+  if est_vide score_match_ch then
+    0
+  else
+    if int_of_string(prem_lettre score_match_ch) > int_of_string(prem_lettre(reste_mot score_match_ch) )then
+      1
+    else
+      2;;
+
+let rec score_match = function marque_match ->
+  if est_vide marque_match then
+    ""
+  else
+    let result_set = score_set (prem_set marque_match)in
+    result_set ^ " " ^ score_match (reste_set marque_match);;
+
+
+let rec compte_set_gagnee = function marque_match -> function j1->
+  if est_vide marque_match then
+    0
+  else
+    if j_gagne(prem_set marque_match) = j1 then
+      1+ compte_set_gagnee (reste_set marque_match) j1
+    else
+      compte_set_gagnee (reste_set marque_match) j1
+
+
+
+let gagne_match = function marque_match -> function nb_set_match -> function j1 -> function j2 ->
+  let score = score_match marque_match in
+  let score_j1 = compte_set_gagnee score 1 in
+  let score_j2 = compte_set_gagnee score 2 in
+  if score_j1 >= nb_set_match then
+    j1
+  else
+  if score_j2 >= nb_set_match then
+    j2
+else
+"Match en cours - " ^ score_match marque_match;;
